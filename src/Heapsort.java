3ca559fddc13dbdba58ff93837bc070e072fe111
wilsontheory <wilson.brian90@gmail.com>
2017-04-06 04:14:03 -0400

    fixed the structure of Heapsort, which was running much slower due to the way heap was constructed

--- src/Heapsort.java
+++ src/Heapsort.java
@@ -2,126 +2,69 @@ import java.util.Arrays;
 
 public final class Heapsort {
 	static int[] heapArray;
-	
+	static int heapArrayLength;
 
 	public static void run(int[] inputArray) {
+		// clone the input array into a heapArray we can work with
 		heapArray = inputArray.clone();
-		int inputArrayLength = inputArray.length;
-		
-		//start the timer
+		heapArrayLength = inputArray.length;
+
+		// start the timer
 		long startTime = System.nanoTime();
-		
-		//build the heap
-		buildMinHeap(heapArray);
-		
-		//pull from the heap, replacing each value of inputArray as iteration proceeds
-		for (int idx = 0; idx < inputArrayLength; idx++){
-			inputArray[idx] = pullRoot(heapArray);
+
+		// build the heap
+		heapify(heapArray);
+
+		// pull from the heap, replacing each value of inputArray as iteration
+		int ctr = 0;
+		//counter for inputArray index value
+		for (int idx = heapArrayLength - 1; idx >= 0; idx--) {
+			inputArray[ctr] = heapArray[0];
+			ctr++;
+			heapArray[0] = heapArray[idx];
+			minHeap(heapArray, 0);
+			heapArrayLength--;
 		}
-		
-		//end the timer, record/print
+
+		// end the timer, record/print
 		long endTime = System.nanoTime() - startTime;
 		System.out.println("Heap Sort runtime (ns) = " + endTime);
 
 	}
-	
-	private static int pullRoot(int[] array){
-		//save root value, swap it with the last element in the tree
-		int rootValue = array[0];
-		int arrayLength = array.length;
-		//shorter array will be filled with values after heapify
-		int[] shorterArray = new int[arrayLength - 1];
-		
-		array[0] = array[arrayLength - 1];
-		
-		//heapify to trickle the new value down
-		heapify(array, 0);
-		
-		//alter the original array ("testArray") by copying a 1-shorter version
-		System.arraycopy(array, 0, shorterArray, 0, (arrayLength - 1));
-		heapArray = shorterArray;
 
+	private static void minHeap(int[] array, int idx) {
+		// this currently works with min heap, keeping small at top of heap
+		int idxLeft = (idx * 2) + 1;
+		int idxRight = (idx * 2) + 2;
+		int minimum = idx;
 		
-//		System.arraycopy(array, 0, testArray, 0, arrayLength - 1);
-		return rootValue;
-	}
-	
-	private static void heapify(int[] array, int idx){
-		//this currently works with min heap, keeping small at top of heap
-		int arrayLength = array.length;
-		int idxLeftChild;
-		int idxRightChild;
-		int temp;
-		
-		if ((idx * 2) + 1 <= arrayLength -1){
-			//if the left child exists, compare and swap if necessary, then recursively call heapify
-			idxLeftChild = idx * 2 + 1;
-			if (array[idx] > array[idxLeftChild]){
-				temp = array[idx];
-				array[idx] = array[idxLeftChild];
-				array[idxLeftChild] = temp;
-				heapify(array, idxLeftChild);
-				//the left child was swapped with parent, so call heapifiy on THAT
-			}
+		if (idxLeft < heapArrayLength && array[idxLeft] < array[idx]){
+			minimum = idxLeft;
 		}
 		
-		if ((idx * 2) + 2 <= arrayLength - 1){
-			//if the right child exists, compare and swap if necessary, then recursively call heapify
-			idxRightChild = idx * 2 + 2;
-			if (array[idx] > array[idxRightChild]){
-				temp = array[idx];
-				array[idx] = array[idxRightChild];
-				array[idxRightChild] = temp;
-				heapify(array, idxRightChild);
-				//the right child was swapped with parent, call heapify on THAT
-			}
+		if (idxRight < heapArrayLength && array[idxRight] < array[minimum]){
+			minimum = idxRight;
 		}
+		
+		if (idx != minimum){
+			swap(array, idx, minimum);
+			minHeap(array, minimum);
+		}
+		
 
 	}
+	
+	private static void swap(int[] array, int i, int j){
+		//helper method to swap the values at two indices
+		int temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
 
-	private static void buildMinHeap(int[] array) {
-		int arrayLength = array.length;
-		boolean sortedFlag = false;
+	private static void heapify(int[] array) {
 
-		while (!sortedFlag) {
-			sortedFlag = true;
-			for (int idx = 0; idx < array.length; idx++) {
-
-				int indexOfLeftChild;
-				int indexOfRightChild;
-				int temp;
-
-				if ((idx * 2) + 1 <= arrayLength - 1) {
-					// check if left child exists, if so compare and swap if
-					// necessary
-					indexOfLeftChild = idx * 2 + 1;
-					if (array[idx] > array[indexOfLeftChild]) {
-						// if the left child is smaller than parent, swap the
-						// values
-						temp = array[idx];
-						array[idx] = array[indexOfLeftChild];
-						array[indexOfLeftChild] = temp;
-						sortedFlag = false;
-					}
-				}
-
-				if ((idx * 2) + 2 <= arrayLength - 1) {
-					// check if right child exists, if so compare and swap if
-					// necessary
-					indexOfRightChild = idx * 2 + 2;
-					if (array[idx] > array[indexOfRightChild]) {
-						// if the right child is smaller than parent, swap the
-						// values
-						temp = array[idx];
-						array[idx] = array[indexOfRightChild];
-						array[indexOfRightChild] = temp;
-						sortedFlag = false;
-					}
-				}
-
-				// end?
-			}
-
+		for (int idx = heapArrayLength; idx >= 0; idx--) {
+			minHeap(array, idx);
 		}
 
 	}
